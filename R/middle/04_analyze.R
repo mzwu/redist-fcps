b609bddaab97f93f74e4bffa2cb170b33966e3bd
Melissa Wu <melizwu@holy8a28109.rc.fas.harvard.edu>
2026-01-26 17:36:13 -0500

    only 0 inc

--- R/middle/04_analyze.R
+++ R/middle/04_analyze.R
@@ -10,5 +10,5 @@ validate_analysis(plans, map, "middle")
 summary(plans)
 
 plans_thin <- plans %>%
-  filter(school_outside_zone %in% c(0, 1))
-write_rds(plans, here(paste0("data-raw/middle/plans/plans_ms_com1_inc11_split0_cap1_pop0.2_thin.rds")))
+  filter(school_outside_zone == 0)
+write_rds(plans, here(paste0("data-raw/middle/plans/plans_ms_com1_inc11_split0_cap1_pop0.2_thin0.rds")))
